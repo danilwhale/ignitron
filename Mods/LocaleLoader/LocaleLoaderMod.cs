0697b2b1d4eb9f79f4b553ed9eb5e809b3021a72
danil <61111955+danilwhale@users.noreply.github.com>
2025-08-03 23:33:59 +0300

    chore(mods/localeloader): init

---
+++ Mods/LocaleLoader/LocaleLoaderMod.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Ignitron.Loader.API;
+
+namespace LocaleLoader;
+
+public sealed class LocaleLoaderMod : Mod
+{
+    public override void Initialize()
+    {
+        Harmony harmony = new($"{Metadata.Author}.{Metadata.Name}");
+        harmony.PatchAll();
+    }
+}
---
+++ Mods/LocaleLoader/Patches/GamePatch.cs
@@ -0,0 +1,41 @@
+using System.Globalization;
+using HarmonyLib;
+using PocketBlocks;
+using PocketBlocks.DataManagement.Translation;
+
+namespace LocaleLoader.Patches;
+
+[HarmonyPatch(typeof(Game))]
+public static class GamePatch
+{
+    [HarmonyPatch(nameof(Game.SetupGame))]
+    [HarmonyTranspiler]
+    public static IEnumerable<CodeInstruction> SetupGamePatch(IEnumerable<CodeInstruction> instructions)
+    {
+        foreach (CodeInstruction instruction in instructions)
+        {
+            if (instruction.Calls(
+                    SymbolExtensions.GetMethodInfo<string>(translation => Translator.LoadTranslation(translation))))
+            {
+                yield return CodeInstruction.Call<string>(translation => TryLoadNativeTranslation(translation));
+                continue;
+            }
+
+            yield return instruction;
+        }
+    }
+
+    public static void TryLoadNativeTranslation(string fallbackCode)
+    {
+        CultureInfo systemCulture = CultureInfo.InstalledUICulture;
+        string code = systemCulture.Name.ToLowerInvariant();
+        code = $"{code[..2]}_{code[3..]}";
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "res", "translations", code + ".txt");
+        if (!File.Exists(path))
+        {
+            Console.WriteLine("Locale Loader: Game doesn't have translation for {0}! Falling back to {1}", code, fallbackCode);
+            Translator.LoadTranslation(fallbackCode);
+        }
+        else Translator.LoadTranslation(code);
+    }
+}
