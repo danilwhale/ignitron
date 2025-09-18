3a5742ac355e66e2315d5c154a575c6173f79faa
danil <61111955+danilwhale@users.noreply.github.com>
2025-09-18 15:03:36 +0300

    feat(loader): fix gross code

--- Ignitron.Loader/Entrypoint.cs
+++ Ignitron.Loader/Entrypoint.cs
@@ -1,19 +1,14 @@
 using Allumeria;
-using System.Reflection;
 using HarmonyLib;
 using Ignitron.Loader.API;
+using OpenTK.Graphics.OpenGL4;
 
 namespace Ignitron.Loader;
 
 public class Entrypoint
 {
-    private sealed class LoadStateProgressDisplay(object game) : IProgressDisplay
+    private sealed class LoadStateProgressDisplay(Game game) : IProgressDisplay
     {
-        private static readonly FieldInfo LoadStateField = AccessTools.DeclaredField("Allumeria.Game:loadState");
-        private static readonly MethodInfo GlClear = AccessTools.DeclaredMethod("OpenTK.Graphics.OpenGL4.GL:Clear");
-        private static readonly MethodInfo RenderLoadScreenMethod = AccessTools.DeclaredMethod("Allumeria.Game:RenderLoadScreen");
-        private static readonly MethodInfo SwapBuffersMethod = AccessTools.DeclaredMethod("OpenTK.Windowing.Desktop.GameWindow:SwapBuffers");
-
         private string? _category;
         private string? _message;
 
@@ -27,52 +22,44 @@ public class Entrypoint
         {
             if (_message is not null)
             {
-                LoadStateField.SetValue(game, _category is null ? _message : $"{_category}: {_message}");
+                game.loadState = _category is null ? _message : $"{_category}: {_message}";
             }
             else if (_category is not null)
             {
-                LoadStateField.SetValue(game, $"{_category}...");
+                game.loadState = $"{_category}...";
             }
             else
             {
-                LoadStateField.SetValue(game, null);
+                game.loadState = string.Empty;
             }
 
             // now render loading screen
-            GlClear.Invoke(null, [0x100 | 0x4000 /* GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT */]);
-            RenderLoadScreenMethod.Invoke(game, null);
-            SwapBuffersMethod.Invoke(game, null);
+            GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
+            game.RenderLoadScreen();
+            game.SwapBuffers();
 
             _message = message;
         }
     }
 
-    private static class GamePatch
+    [HarmonyPatch(typeof(Game), "OnLoad")]
+    private static class GameLoadPatch
     {
-        [HarmonyPatch]
-        private static class OnLoadPatch
+        public static void Postfix(Game __instance)
         {
-            public static MethodBase TargetMethod()
-            {
-                return AccessTools.DeclaredMethod("Allumeria.Game:OnLoad");
-            }
+            Logger.Init("testicular tortion");
 
-            public static void Postfix(object __instance)
-            {
-                Logger.Init("testicular tortion");
+            // get version field from loaded assembly
+            string fullVersion = Game.VERSION;
+            Logger.Init($"Game version: {fullVersion}");
 
-                // get version field from loaded assembly
-                string fullVersion = Game.VERSION!;
-                Logger.Init($"Game version: {fullVersion}");
+            // append '/ignitron {ver}' so you can identify presence of the modloader
+            Game.VERSION = fullVersion + $"/ignitron {ModLoader.Version}";
 
-                // append '/ignitron {ver}' so you can identify presence of the modloader
-                Game.VERSION = fullVersion + $"/ignitron {ModLoader.Version}";
-
-                // get just version from full version (game stage + version)
-                Version version = Version.Parse(fullVersion.AsSpan(fullVersion.IndexOfAny(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])));
-                if (version.Revision < 0) version = new Version(version.Major, version.Minor, version.Build, 0);
-                ModLoader.Load(new LoadStateProgressDisplay(__instance), Path.Combine(Directory.GetCurrentDirectory(), "mods"), version);
-            }
+            // get just version from full version (game stage + version)
+            Version version = Version.Parse(fullVersion.AsSpan(fullVersion.IndexOfAny(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])));
+            if (version.Revision < 0) version = new Version(version.Major, version.Minor, version.Build, 0);
+            ModLoader.Load(new LoadStateProgressDisplay(__instance), Path.Combine(Directory.GetCurrentDirectory(), "mods"), version);
         }
     }
 
