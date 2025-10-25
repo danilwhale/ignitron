47454c36e2889a0f622a5f43932cb0c0ec5bd2bf
danil <61111955+danilwhale@users.noreply.github.com>
2025-10-25 15:46:30 +0300

    feat(loader): display mod count in main menu

---
+++ Ignitron.Loader/Patches/MainMenuPatches.cs
@@ -0,0 +1,24 @@
+using Allumeria.UI.Menus;
+using HarmonyLib;
+
+namespace Ignitron.Loader.Patches;
+
+[HarmonyPatch]
+internal static class MainMenuPatches
+{
+    [HarmonyPatch(typeof(MainMenu), nameof(MainMenu.Render))]
+    private static class RenderPatch
+    {
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            return new CodeMatcher(instructions)
+                .MatchStartForward(CodeMatch.Calls(() => string.Concat(null, null, null)))
+                .ThrowIfInvalid("couldn't find string.Concat(string, string, string)")
+                .InsertAfter(CodeInstruction.Call(() => AppendModCount(null)))
+                .Instructions();
+        }
+
+        private static string AppendModCount(string brandingText)
+            => $"{brandingText}({IgnitronLoader.Instance.Mods.Count} mods)";
+    }
+}
