ac4e2d5385167f2e3f9ccf47acff2190c39615fa
danil <61111955+danilwhale@users.noreply.github.com>
2025-10-25 16:12:46 +0300

    feat: include loader components as mods

---
+++ Ignitron.Loader/Metadata/Components/IgnitronModMetadata.cs
@@ -0,0 +1,13 @@
+namespace Ignitron.Loader.Metadata.Internal;
+
+internal sealed class IgnitronModMetadata : IModMetadata
+{
+    public string AssemblyRelativePath => "Loader.dll";
+    public string Id => "ignitron";
+    public string DisplayName => "Ignitron";
+    public IEnumerable<IModContributor> Contributors { get; } = [new ModContributor("danilwaffle", "developer")];
+    public string? Description => "A mod loader for Allumeria";
+    public Version Version => IgnitronLoader.Version;
+    public IEnumerable<IModDependency> Dependencies { get; } = [];
+    public IEnumerable<string> Entrypoints { get; } = [];
+}
